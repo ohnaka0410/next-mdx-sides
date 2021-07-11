242ed70db7dff2c7e6b912879d4c77ef6bb2e28f
yuki0410_ <>
2021-07-11 17:31:43 +0900

    no message

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,9 @@
 import "~/styles/globals.css";
 import type { AppProps } from "next/app";
 
+/**
+ * App Component
+ */
 const App: React.VFC<AppProps> = ({ Component, pageProps }): JSX.Element => {
   return <Component {...pageProps} />;
 };
